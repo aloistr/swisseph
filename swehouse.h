306141912e118c33a3ccad2ac5d1ea72a7751cd0
Alois Treindl <alois@as80.astro.ch>
2020-08-05 12:35:03 +0000

    version 2.10   December 2020

--- swehouse.h
+++ swehouse.h
@@ -60,14 +60,26 @@ house and (simple) aspect calculation
 
 struct houses {
 	  double cusp[37];
+	  double cusp_speed[37];
 	  double ac;
+	  double ac_speed;	// speed of ac
 	  double mc;
+	  double mc_speed;	// speed of mc
+	  double armc_speed;	// speed of armc
 	  double vertex;
+	  double vertex_speed;	// speed of vertex
 	  double equasc;
+	  double equasc_speed;	// speed
 	  double coasc1;
+	  double coasc1_speed;	// speed
 	  double coasc2;
+	  double coasc2_speed;	// speed
 	  double polasc;
+	  double polasc_speed;	// speed
 	  double sundec;	// declination of Sun for Sunshine houses
+	  AS_BOOL do_speed;
+	  AS_BOOL do_hspeed;
+	  AS_BOOL do_interpol;
 	  char serr[AS_MAXCH];
 	};
 

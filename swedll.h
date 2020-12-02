0fc7b6b078deb2d2cadb6d17873e1d57c158b26c
Alois Treindl <alois@as80.astro.ch>
2020-12-02 10:45:09 +0000

    version 2.10   December 2020

--- swedll.h
+++ swedll.h
@@ -107,6 +107,10 @@ DllImport int32 CALL_CONV_IMP swe_calc(
         double tjd, int ipl, int32 iflag, 
         double *xx,
         char *serr);
+DllImport int32 CALL_CONV_IMP  swe_calc_pctr(
+        double tjd, int32 ipl, int32 iplctr, int32 iflag, 
+	double *xxret, 
+	char *serr);
 
 DllImport int32 CALL_CONV_IMP swe_calc_ut( 
         double tjd_ut, int32 ipl, int32 iflag, 
@@ -153,9 +157,17 @@ DllImport int  CALL_CONV_IMP swe_houses_ex(
         double tjd_ut, int32 iflag, double geolat, double geolon, int hsys, 
         double *hcusps, double *ascmc);
 
+DllImport int  CALL_CONV_IMP swe_houses_ex2(
+        double tjd_ut, int32 iflag, double geolat, double geolon, int hsys, 
+        double *hcusps, double *ascmc, double *cusp_speed, double *ascmc_speed, char *serr);
+
 DllImport int  CALL_CONV_IMP swe_houses_armc(
         double armc, double geolat, double eps, int hsys, 
         double *hcusps, double *ascmc);
+
+DllImport int  CALL_CONV_IMP swe_houses_armc_ex2(
+        double armc, double geolat, double eps, int hsys, 
+        double *hcusps, double *ascmc, double *cusp_speed, double *ascmc_speed, char *serr);
 
 DllImport double  CALL_CONV_IMP swe_house_pos(
         double armc, double geolon, double eps, int hsys, double *xpin, char *serr);
@@ -175,6 +187,7 @@ DllImport double  CALL_CONV_IMP swe_get_ayanamsa(double tjd_et);
 DllImport double  CALL_CONV_IMP swe_get_ayanamsa_ut(double tjd_ut);
 
 DllImport char * CALL_CONV_IMP swe_get_ayanamsa_name(int32 isidmode);
+DllImport char * CALL_CONV_IMP swe_get_current_file_data(int ifno, double *tfstart, double *tfend, int *denum);
 
 DllImport int  CALL_CONV_IMP swe_date_conversion(
         int y , int m , int d ,         /* year, month, day */

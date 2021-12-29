580047f82c11b59a5ad9f454594d796b48808d90
Alois Treindl <alois@astro.ch>
2021-12-29 11:47:36 +0100

    improved code, so that it works without input

--- swemini.c
+++ swemini.c
@@ -4,75 +4,29 @@
   swemini.c	A minimal program to test the Swiss Ephemeris.
 
   Input: a date (in gregorian calendar, sequence day.month.year)
+  	if no date is entered, 1 Jan 2022 is used. Next time, the date
+	advances by one day.
   Output: Planet positions at midnight Universal time, ecliptic coordinates,
           geocentric apparent positions relative to true equinox of date, as 
           usual in western astrology.
 		
    
-  Authors: Dieter Koch and Alois Treindl, Astrodienst Zurich
+  Authors: Dieter Koch and Alois Treindl.
+
+  The code of sample program swemini.c is in the public domain.
+  (But not the code of the library functions called by it.)
 
 **************************************************************/
-/* Copyright (C) 1997 - 2021 Astrodienst AG, Switzerland.  All rights reserved.
-
-  License conditions
-  ------------------
-
-  This file is part of Swiss Ephemeris.
-
-  Swiss Ephemeris is distributed with NO WARRANTY OF ANY KIND.  No author
-  or distributor accepts any responsibility for the consequences of using it,
-  or for whether it serves any particular purpose or works at all, unless he
-  or she says so in writing.  
-
-  Swiss Ephemeris is made available by its authors under a dual licensing
-  system. The software developer, who uses any part of Swiss Ephemeris
-  in his or her software, must choose between one of the two license models,
-  which are
-  a) GNU Affero General Public License (AGPL)
-  b) Swiss Ephemeris Professional License
-
-  The choice must be made before the software developer distributes software
-  containing parts of Swiss Ephemeris to others, and before any public
-  service using the developed software is activated.
-
-  If the developer choses the AGPL software license, he or she must fulfill
-  the conditions of that license, which includes the obligation to place his
-  or her whole software project under the AGPL or a compatible license.
-  See https://www.gnu.org/licenses/agpl-3.0.html
-
-  If the developer choses the Swiss Ephemeris Professional license,
-  he must follow the instructions as found in http://www.astro.com/swisseph/ 
-  and purchase the Swiss Ephemeris Professional Edition from Astrodienst
-  and sign the corresponding license contract.
-
-  The License grants you the right to use, copy, modify and redistribute
-  Swiss Ephemeris, but only under certain conditions described in the License.
-  Among other things, the License requires that the copyright notices and
-  this notice be preserved on all copies.
-
-  Authors of the Swiss Ephemeris: Dieter Koch and Alois Treindl
-
-  The authors of Swiss Ephemeris have no control or influence over any of
-  the derived works, i.e. over software or services created by other
-  programmers which use Swiss Ephemeris functions.
-
-  The names of the authors or of the copyright holder (Astrodienst) must not
-  be used for promoting any software, product or service which uses or contains
-  the Swiss Ephemeris. This copyright notice is the ONLY place where the
-  names of the authors can legally appear, except in cases where they have
-  given special permission in writing.
-
-  The trademarks 'Swiss Ephemeris' and 'Swiss Ephemeris inside' may be used
-  for promoting such software, products or services.
-*/
 
 
-#include "swephexp.h" 	/* this includes  "sweodef.h" */
+#include "swephexp.h" 	
+
+char *smon[] = {NULL, "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
 int main()
 {
   char sdate[AS_MAXCH], snam[40], serr[AS_MAXCH];  
-  int jday = 1, jmon = 1, jyear = 2000;
+  int jday = 1, jmon = 1, jyear = 2022;
   double jut = 0.0;
   double tjd, te, x2[6];
   int32 iflag, iflgret;
@@ -81,51 +35,39 @@ int main()
   iflag = SEFLG_SPEED;
   while (TRUE) {
     printf("\nDate (d.m.y) ?");
-    /*gets(sdate);*/
-    if( !fgets(sdate, sizeof(sdate)-1, stdin) ) return OK;
-		/*
-		 * stop if a period . is entered
-		 */
+    fgets(sdate, sizeof(sdate)-1, stdin);
+    // stop if a period . is entered
     if (*sdate == '.') 
       return OK;
-    if (sscanf (sdate, "%d%*c%d%*c%d", &jday,&jmon,&jyear) < 1) exit(1);
-	    /*
-	     * we have day, month and year and convert to Julian day number
-	     */
+    sscanf (sdate, "%d%*c%d%*c%d", &jday,&jmon,&jyear);
+    // we have day, month and year and convert to Julian day number
+    if (jmon < 1 || jmon > 12) {
+      printf("illegal month %d\n", jmon);
+      continue;
+    }
     tjd = swe_julday(jyear,jmon,jday,jut,SE_GREG_CAL);        
-	    /*
-	     * compute Ephemeris time from Universal time by adding delta_t
-	     */
+    // compute Ephemeris time from Universal time by adding delta_t
     te = tjd + swe_deltat(tjd);
-    printf("date: %02d.%02d.%d at 0:00 Universal time\n", jday, jmon, jyear);
+    printf("date: %02d %s %04d at 0:00 Universal time, jd=%.1lf\n", jday, smon[jmon], jyear, tjd);
     printf("planet     \tlongitude\tlatitude\tdistance\tspeed long.\n");
-	    /*
-	     * a loop over all planets
-	     */
-    for (p = SE_SUN; p <= SE_CHIRON; p++) {
+    for (p = SE_SUN; p <= SE_CHIRON; p++) { // a loop over all planets
       if (p == SE_EARTH) continue;
-		/*
-		 * do the coordinate calculation for this planet p
-		 */
+      swe_get_planet_name(p, snam); //  get the name of the planet p
+      // do the coordinate calculation for this planet p
       iflgret = swe_calc(te, p, iflag, x2, serr);
-	      /*
-	       * if there is a problem, a negative value is returned and an 
-	       * errpr message is in serr.
-	       */
-      if (iflgret < 0) 
-	printf("error: %s\n", serr);
-      else if (iflgret != iflag)
+      // if there is a problem, a negative value is returned and an error message is in serr.
+      if (iflgret < 0) {
+	printf("%10s\terror: %s\n", snam, serr);
+	continue;
+      }
+      if (iflgret != iflag)
 	printf("warning: iflgret != iflag. %s\n", serr);
-	      /*
-	       * get the name of the planet p
-	       */
-      swe_get_planet_name(p, snam);
-	      /*
-	       * print the coordinates
-	       */
+      // print the coordinates
       printf("%10s\t%11.7f\t%10.7f\t%10.7f\t%10.7f\n",
 	     snam, x2[0], x2[1], x2[2], x2[3]);
     }
+    tjd++;	// if date entry is empty, take next day
+    swe_revjul(tjd, SE_GREG_CAL, &jyear, &jmon, &jday, &jut);
   }
   return OK;
 }
